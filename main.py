5d6d474a0c46a30adfb2ae70756c7a0b781e4a88
Andre Silveira <ssilveira.andre@outlook.com>
2023-12-09 14:49:02 -0300

    Fourth commit - First step to Payments function

--- main.py
+++ main.py
@@ -21,11 +21,33 @@ def fechar_conexao(conexao):
 # validate the API Token here 
 bot = telebot.TeleBot(token)
 
+DT_CADASTRO_CLI = (
+    "CEA"
+)
+DT_CADASTRO_CONTRATO = (
+    "MOVIDA_PF",
+    "MOVIDA_MEMO",
+    "MOVIDA_JURIDICO"
+    "VOLKSWAGEN",
+    "TORRA_TORRA",
+    "SUPERLOGICA",
+    "SAMSUNG"
+)
+DT_CADASTRO_CONT = (
+    "PORTO_SEGURO_CARRO_FACIL",
+    "JSL"
+)
+DT_CADASTRO_CON = (
+    "PORTO_SEGURO_SEM_GARANTIA",
+    "SICREDI_PIQUERI"
+)
+
+
 # Function that verifies if any message is inputted here 
 def verificar(mensagem):
     return True
 
-def execCea(mensagem, carteira, conexao, cursor):
+def execCarteira(mensagem, carteira, conexao, cursor):
     try:
         bot.send_message(mensagem.chat.id, f'Atualização da base de clientes da {carteira} em andamento...')
         query_exec = pd.read_sql_query(f"select nm_prc_tabela from tb_tabela_visao_carteira where nm_carteira = '{carteira}'",conexao)
@@ -36,27 +58,75 @@ def execCea(mensagem, carteira, conexao, cursor):
         # recebe nome da tabela pela variavel 
         query_select_nm_tabela = pd.read_sql_query(f"select nm_tabela from tb_tabela_visao_carteira where nm_carteira = '{carteira}'",conexao)
         # conta contratos a partir da tabela 
-        if carteira == 'CEA':
+        if carteira in DT_CADASTRO_CLI:
             query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(dt_cadastro_cli as date) = cast(getdate() as date)",conexao)
             bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} clientes novos!")               
-        if carteira == 'MOVIDA_PF':
+        
+        if carteira in DT_CADASTRO_CONTRATO:
             query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(dt_cadastro_contrato as date) = cast(getdate() as date)",conexao)
             bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
-        if carteira == 'PORTO_SEGURO_CARRO_FACIL':
+        
+        if carteira in DT_CADASTRO_CONT:
             query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(DT_CADASTRO_CONT as date) = cast(getdate() as date)",conexao)
             bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
 
+        if carteira in DT_CADASTRO_CON:
+            query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(DT_CADASTRO_CON as date) = cast(getdate() as date)",conexao)
+            bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
+
         
+
     except Exception as e: 
         bot.send_message(mensagem.chat.id, f'Ocorreu um erro: {str(e)}')
     finally:
         fechar_conexao(conexao)
 
-@bot.message_handler(commands=["CEA","MOVIDA_PF"])
+def execPagamentos(mensagem, carteira, conexao, cursor):
+    try:
+        # bot.send_message(mensagem.chat.id, f'Atualização da base de pagamentos da {carteira} em andamento...')
+        # query_exec = pd.read_sql_query(f"select nm_prc_tabela from tb_tabela_visao_carteira where nm_carteira = '{carteira}'",conexao)
+        # cursor.execute('exec ' + query_exec.iloc[0,0])
+        # cursor.commit()        
+        
+        # bot.send_message(mensagem.chat.id, 'Base de pagamentos atualizada!')
+        # recebe nome da tabela pela variavel 
+        # query_select_nm_tabela = pd.read_sql_query(f"select nm_tabela from tb_tabela_visao_carteira where nm_carteira = '{carteira}'",conexao)
+        # conta contratos a partir da tabela 
+        # if carteira in DT_CADASTRO_CLI:
+        #     query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(dt_cadastro_cli as date) = cast(getdate() as date)",conexao)
+        #     bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} clientes novos!")               
+        
+        # if carteira in DT_CADASTRO_CONTRATO:
+        #     query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(dt_cadastro_contrato as date) = cast(getdate() as date)",conexao)
+        #     bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
+        
+        # if carteira in DT_CADASTRO_CONT:
+        #     query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(DT_CADASTRO_CONT as date) = cast(getdate() as date)",conexao)
+        #     bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
+
+        # if carteira in DT_CADASTRO_CON:
+        #     query_cnt = pd.read_sql_query(f"select count(distinct id_contrato) from {query_select_nm_tabela.iloc[0,0]} where cast(DT_CADASTRO_CON as date) = cast(getdate() as date)",conexao)
+        #     bot.send_message(mensagem.chat.id,f"{query_cnt.iloc[0,0]} contratos novos!")               
+
+        pass 
+
+    except Exception as e: 
+        bot.send_message(mensagem.chat.id, f'Ocorreu um erro: {str(e)}')
+    finally:
+        fechar_conexao(conexao)        
+
+@bot.message_handler(commands=["CEA","MOVIDA_PF","MOVIDA_MEMO","MOVIDA_JURIDICO","PORTO_SEGURO_CARRO_FACIL","PORTO_SEGURO_SEM_GARANTIA","VOLKSWAGEN","TORRA_TORRA","SUPERLOGICA","SICREDI_PIQUERI","SAMSUNG","JSL"])
 def processa_carteira(mensagem):
     conexao, cursor = conecta_banco()
     carteira_selecionada = mensagem.text[1:] 
-    execCea(mensagem, carteira_selecionada, conexao, cursor)
+    execCarteira(mensagem, carteira_selecionada, conexao, cursor)
+
+@bot.message_handler(commands=["CEA","MOVIDA_PF","MOVIDA_MEMO","MOVIDA_JURIDICO","PORTO_SEGURO_CARRO_FACIL","PORTO_SEGURO_SEM_GARANTIA","VOLKSWAGEN","TORRA_TORRA","SUPERLOGICA","SICREDI_PIQUERI","SAMSUNG","JSL"])
+def processa_pagamentos(mensagem):
+    conexao, cursor = conecta_banco()
+    carteira_selecionadap = mensagem.text[1:] 
+    execPagamentos(mensagem, carteira_selecionadap, conexao, cursor)
+
 
 # if visao_carteira is marked here
 @bot.message_handler(commands=["Carteira"])
@@ -66,9 +136,39 @@ def opcao1(mensagem):
     Selecione a carteira 
 /CEA
 /MOVIDA_PF    
+/MOVIDA_MEMO
+/MOVIDA_JURIDICO
 /PORTO_SEGURO_CARRO_FACIL
+/PORTO_SEGURO_SEM_GARANTIA
+/VOLKSWAGEN
+/TORRA_TORRA
+/SUPERLOGICA
+/SICREDI_PIQUERI
+/SAMSUNG
+/JSL
 """
     bot.send_message(mensagem.chat.id, textovc)
+
+# if pagamentos is marked here
+@bot.message_handler(commands=["Pagamentos"])
+def opcao1(mensagem):
+    conexao, cursor = conecta_banco()
+    textop = """
+    Selecione a carteira 
+/CEA
+/MOVIDA_PF    
+/MOVIDA_MEMO
+/MOVIDA_JURIDICO
+/PORTO_SEGURO_CARRO_FACIL
+/PORTO_SEGURO_SEM_GARANTIA
+/VOLKSWAGEN
+/TORRA_TORRA
+/SUPERLOGICA
+/SICREDI_PIQUERI
+/SAMSUNG
+/JSL
+"""
+    bot.send_message(mensagem.chat.id, textop)
 
 # Primary menu here 
 @bot.message_handler(func=verificar)
